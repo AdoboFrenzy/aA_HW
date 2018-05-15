99c3f1e66fddc61a22241ae374a121341b7f9745
Alfred Alejandrino <alejandrino.alfred@gmail.com>
2018-05-14 21:51:57 -0700

    finish W5D2 homework

--- W5D2/Poly_Concern/app/models/cat.rb
+++ W5D2/Poly_Concern/app/models/cat.rb
@@ -1,2 +1,12 @@
 class Cat < ApplicationRecord
+  include Toyable
+  # has_many :toys, as: :toyable
+
+
+  # we don't need the ones below with polymorphic association
+  # primary_key: :id,
+  # foreign_key: :cat_id,
+  # class_name: :Toy
+
+
 end
--- W5D2/Poly_Concern/app/models/corgi.rb
+++ W5D2/Poly_Concern/app/models/corgi.rb
@@ -1,2 +1,10 @@
 class Corgi < ApplicationRecord
+  include Toyable
+  # has_many :toys, as: :toyable
+
+  # we don't need the ones below with polymorphic association
+  # primary_key: :id,
+  # foreign_key: :corgi_id,
+  # class_name: :Toy
+
 end
