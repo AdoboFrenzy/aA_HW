3b046db95f966da431759b9eab38962d9398ed0f
Alfred Alejandrino <alejandrino.alfred@gmail.com>
2018-05-25 00:00:47 -0700

    initial add webpack setup

---
+++ W6D5/Nodepack_Manager/webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./entry.jsx",
+  output: {
+      path: path.resolve(__dirname),
+      filename: "bundle.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react']
+          }
+        },
+      }
+    ]
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+  extensions: ['.js', '.jsx', '*']
+},
+
+};
+
+// above are snippets from Webpack Configuration reading
