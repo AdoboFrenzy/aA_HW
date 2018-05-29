6700a9459ff290f22244df022b2d5f112d830f56
Alfred Alejandrino <alejandrino.alfred@gmail.com>
2018-05-28 21:48:30 -0700

    add W7D2 hw

---
+++ W7D2/intro_middleware/frontend/store/store.js
@@ -0,0 +1,50 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+
+    applyMiddleware(addLoggingToDispatch, anotherMidware)
+  );
+
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+  return store;
+};
+
+
+
+const addLoggingToDispatch = store => next => action => {
+
+  console.log(store.getState());
+  console.log(action);
+
+  next(action);
+  console.log(store.getState());
+};
+
+const anotherMidware = store => next => action => {
+
+  console.log("another midware");
+
+  next(action);
+};
+
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+//
+//   middlewares.forEach((mWare) => {
+//     dispatch = mWare(store)(dispatch);
+//   });
+//
+//   return (Object.assign({}, store, { dispatch }));
+// }
+
+
+
+
+
+export default configureStore;
