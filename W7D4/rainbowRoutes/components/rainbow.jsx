6697c12e0c00d6a5dac8268330d722f1b3b42c2e
Alfred Alejandrino <alejandrino.alfred@gmail.com>
2018-05-30 19:13:38 -0700

    initial commit W7D4 hw

---
+++ W7D4/rainbowRoutes/components/rainbow.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+
+import Red from './red';
+import Orange from './orange';
+import Yellow from './yellow';
+import Green from './green';
+import Blue from './blue';
+import Indigo from './indigo';
+import Violet from './violet';
+
+
+class Rainbow extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rainbow Router!</h1>
+          <Link to='/red' >Red</Link>
+          <Link to='/blue' >Blue only</Link>
+          <Link to='/blue/indigo' >Add indigo</Link>
+        {/* Your links should go here */}
+
+        <div id="rainbow">
+          {<Route path="/red" component={Red} />}
+          {<Route path="/red/orange" component={Orange} />}
+          {<Route path="/red/yellow" component={Yellow} />}
+          {<Route path="/green" component={Green} />}
+          {<Route path="/blue" component={Blue} />}
+          {<Route path="/blue/indigo" component={Indigo} />}
+          {<Route path="/violet" component={Violet} />}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Rainbow;
